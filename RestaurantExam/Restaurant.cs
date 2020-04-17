bdfb79ec10c36304dcb2c3e4d7cd8a53a50f899c
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-17 15:59:13 +0100

    Add project files.

---
+++ RestaurantExam/Restaurant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantExam
+{
+    class Restaurant
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        private double gratuity;
+
+        public Restaurant (string name, string address,double grat)
+        {
+            Name = name;
+            Address = address;
+            Gratuity = grat;
+        }
+
+        public double Gratuity
+        {
+            get { return gratuity; }
+            private set
+            {
+                if (value > 0.0)
+               
+                { gratuity = value; }
+
+            }
+        }
+
+        public void GenerateRececipt()
+        {
+
+        }
+
+    }
+}
